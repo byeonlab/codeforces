9e2ffbe4b97b57630ca3c60f468817089fad65a6
Ubuntu <ubuntu@ip-172-31-16-124.ap-northeast-2.compute.internal>
2019-09-19 09:21:01 +0000

    1220B...

---
+++ 1220/B.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int n;
+	int *M, *a;
+
+	scant("%d", &n);
+	M = malloc(sizeof(int) * n * n);
+	a = malloc(sizeof(int) * n);
+
+	scanf("%s", )
+}
